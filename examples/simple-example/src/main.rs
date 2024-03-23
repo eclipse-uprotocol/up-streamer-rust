ed0aad5af7fa0ea55c467a3b8052df72f13df451
Peter LeVasseur <peter.levasseur@gm.com>
2024-03-23 15:56:20 -0400

    fleshed example out more, need to get working

--- examples/simple-example/src/main.rs
+++ examples/simple-example/src/main.rs
@@ -1,12 +1,19 @@
 use crate::up_client_foo::{UPClientFoo, UTransportBuilderFoo};
 use async_std::channel;
 use std::sync::Arc;
-use up_rust::{Number, UAuthority, UMessage, UStatus};
-use up_streamer::{Route, UTransportRouter};
+use up_rust::UMessageType::UMESSAGE_TYPE_PUBLISH;
+use up_rust::{
+    Number, UAttributes, UAuthority, UEntity, UMessage, UPayload, UStatus, UTransport, UUri,
+};
+use up_streamer::{Route, UStreamer, UTransportRouter};
 
 mod up_client_foo;
 
 pub type UtransportListener = Box<dyn Fn(Result<UMessage, UStatus>) + Send + Sync + 'static>;
+
+pub fn listener_fn(received: Result<UMessage, UStatus>) {
+    println!("within listener_fn!");
+}
 
 #[async_std::main]
 async fn main() {
@@ -39,4 +46,72 @@ async fn main() {
         ..Default::default()
     };
     let route_b = Route::new(&client_2_authority, &utransport_router_handle_2);
+
+    let ustreamer = UStreamer::new("my_streamer");
+    let add_forwarding_rule_res = ustreamer
+        .add_forwarding_rule(route_a.clone(), route_b.clone())
+        .await;
+    assert!(add_forwarding_rule_res.is_ok());
+    let add_forwarding_rule_res = ustreamer
+        .add_forwarding_rule(route_b.clone(), route_a.clone())
+        .await;
+    assert!(add_forwarding_rule_res.is_ok());
+
+    let client_1_uuri = UUri {
+        authority: Some(client_1_authority).into(),
+        entity: Some(UEntity {
+            name: "entity_1".to_string(),
+            id: Some(10),
+            version_major: Some(1),
+            ..Default::default()
+        })
+        .into(),
+        ..Default::default()
+    };
+
+    let client_2_uuri = UUri {
+        authority: Some(client_2_authority).into(),
+        entity: Some(UEntity {
+            name: "entity_2".to_string(),
+            id: Some(10),
+            version_major: Some(1),
+            ..Default::default()
+        })
+        .into(),
+        ..Default::default()
+    };
+
+    let register_res = client_1
+        .register_listener(client_2_uuri.clone(), Box::new(listener_fn))
+        .await;
+    let (registration_string_client_1) = register_res else {
+        panic!("Unable to register!");
+    };
+
+    let register_res = client_2
+        .register_listener(client_1_uuri.clone(), Box::new(listener_fn))
+        .await;
+    let (registration_string_client_2) = register_res else {
+        panic!("Unable to register!");
+    };
+
+    let message_from_client_1_for_client_2 = UMessage {
+        attributes: Some(UAttributes {
+            source: Some(client_1_uuri.clone()).into(),
+            type_: UMESSAGE_TYPE_PUBLISH.into(),
+            ..Default::default()
+        })
+        .into(),
+        payload: Some(UPayload {
+            length: None,
+            format: Default::default(),
+            data: None,
+            special_fields: Default::default(),
+        })
+        .into(),
+        ..Default::default()
+    };
+
+    let send_res = client_1.send(message_from_client_1_for_client_2).await;
+    assert!(send_res.is_ok());
 }
