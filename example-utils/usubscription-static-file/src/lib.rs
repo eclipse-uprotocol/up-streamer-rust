4ae52470334229da3c9c819cd179f698f05e1009
Matthew D'Alonzo <matthew.dalonzo@gm.com>
2024-06-25 16:13:57 -0400

    USubscription Trait Implementation for Static File

--- example-utils/usubscription-static-file/src/lib.rs
+++ example-utils/usubscription-static-file/src/lib.rs
@@ -13,18 +13,66 @@
 
 #![allow(clippy::mutable_key_type)]
 
+use async_trait::async_trait;
+use serde_json::Value;
 use std::collections::{HashMap, HashSet};
 use std::fs::{self, canonicalize};
 use std::path::PathBuf;
 use std::str::FromStr;
-use up_rust::UUri;
-use serde_json::Value;
+use up_rust::core::usubscription::{
+    FetchSubscribersRequest, FetchSubscribersResponse, FetchSubscriptionsRequest,
+    FetchSubscriptionsResponse, NotificationsRequest, SubscriptionRequest, SubscriptionResponse,
+    USubscription, UnsubscribeRequest,
+};
+use up_rust::{UStatus, UUri};
 
 pub struct USubscriptionStaticFile {}
 
 impl Default for USubscriptionStaticFile {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[async_trait]
+impl USubscription for USubscriptionStaticFile {
+    async fn subscribe(
+        &self,
+        subscription_request: SubscriptionRequest,
+    ) -> Result<SubscriptionResponse, UStatus> {
+        todo!()
+    }
+
+    async fn fetch_subscriptions(
+        &self,
+        fetch_subscriptions_request: FetchSubscriptionsRequest,
+    ) -> Result<FetchSubscriptionsResponse, UStatus> {
+        todo!()
+    }
+
+    async fn unsubscribe(&self, unsubscribe_request: UnsubscribeRequest) -> Result<(), UStatus> {
+        todo!()
+    }
+
+    async fn register_for_notifications(
+        &self,
+        notifications_register_request: NotificationsRequest,
+    ) -> Result<(), UStatus> {
+        todo!()
+    }
+
+    async fn unregister_for_notifications(
+        &self,
+        notifications_unregister_request: NotificationsRequest,
+    ) -> Result<(), UStatus> {
+        todo!()
+    }
+
+    async fn fetch_subscribers(
+        &self,
+        fetch_subscribers_request: FetchSubscribersRequest,
+    ) -> Result<FetchSubscribersResponse, UStatus> {
+        todo!();
     }
 }
 
@@ -38,69 +86,67 @@ impl USubscriptionStaticFile {
         // This is a static file, so we will just return the same set of subscribers
         // for all URIs
         println!("fetch_subscribers for topic: {}", topic);
-        
+
         let crate_dir = env!("CARGO_MANIFEST_DIR");
         let subscription_json_file = PathBuf::from(crate_dir).join("static-configs/testdata.json");
 
         match canonicalize(subscription_json_file) {
             Ok(subscription_json_file) => {
                 println!("subscription_json_file: {:?}", subscription_json_file);
-                
+
                 match fs::read_to_string(&subscription_json_file) {
-                    Ok(data) => {
-                        match serde_json::from_str::<Value>(&data) {
-                            Ok(res) => {
-                                let mut subscribers_map = HashMap::new();
-                                
-                                if let Some(obj) = res.as_object() {
-                                    for (key, value) in obj {
-                                        println!("key: {}, value: {}", key, value);
-                                        let mut subscriber_set: HashSet<UUri> = HashSet::new();
-                                        
-                                        if let Some(array) = value.as_array() {
-                                            for subscriber in array {
-                                                println!("subscriber: {}", subscriber);
-                                                
-                                                if let Some(subscriber_str) = subscriber.as_str() {
-                                                    match UUri::from_str(subscriber_str) {
-                                                        Ok(uri) => {
-                                                            println!("All good for subscriber");
-                                                            subscriber_set.insert(uri);
-                                                        }
-                                                        Err(error) => {
-                                                            println!("Error with Deserializing Subscriber: {}", error);
-                                                        }
+                    Ok(data) => match serde_json::from_str::<Value>(&data) {
+                        Ok(res) => {
+                            let mut subscribers_map = HashMap::new();
+
+                            if let Some(obj) = res.as_object() {
+                                for (key, value) in obj {
+                                    println!("key: {}, value: {}", key, value);
+                                    let mut subscriber_set: HashSet<UUri> = HashSet::new();
+
+                                    if let Some(array) = value.as_array() {
+                                        for subscriber in array {
+                                            println!("subscriber: {}", subscriber);
+
+                                            if let Some(subscriber_str) = subscriber.as_str() {
+                                                match UUri::from_str(subscriber_str) {
+                                                    Ok(uri) => {
+                                                        println!("All good for subscriber");
+                                                        subscriber_set.insert(uri);
                                                     }
-                                                } else {
-                                                    println!("Unable to parse subscriber");
+                                                    Err(error) => {
+                                                        println!("Error with Deserializing Subscriber: {}", error);
+                                                    }
                                                 }
-                                            }
-                                        }
-                                        
-                                        println!("key: {}", key);
-                                        match UUri::from_str(&key.to_string()) {
-                                            Ok(mut uri) => {
-                                                println!("All good for key");
-                                                uri.resource_id = 0x8001;
-                                                subscribers_map.insert(uri, subscriber_set);
-                                            }
-                                            Err(error) => {
-                                                println!("Error with Deserializing Key: {}", error);
+                                            } else {
+                                                println!("Unable to parse subscriber");
                                             }
                                         }
                                     }
+
+                                    println!("key: {}", key);
+                                    match UUri::from_str(&key.to_string()) {
+                                        Ok(mut uri) => {
+                                            println!("All good for key");
+                                            uri.resource_id = 0x8001;
+                                            subscribers_map.insert(uri, subscriber_set);
+                                        }
+                                        Err(error) => {
+                                            println!("Error with Deserializing Key: {}", error);
+                                        }
+                                    }
                                 }
-                                
-                                println!("{}", res);
-                                dbg!(&subscribers_map);
-                                subscribers_map
                             }
-                            Err(e) => {
-                                eprintln!("Unable to parse JSON: {}", e);
-                                HashMap::new()
-                            }
+
+                            println!("{}", res);
+                            dbg!(&subscribers_map);
+                            subscribers_map
                         }
-                    }
+                        Err(e) => {
+                            eprintln!("Unable to parse JSON: {}", e);
+                            HashMap::new()
+                        }
+                    },
                     Err(e) => {
                         eprintln!("Unable to read file: {}", e);
                         HashMap::new()
