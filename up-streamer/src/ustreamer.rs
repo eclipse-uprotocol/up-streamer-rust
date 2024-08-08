36fa7c5cc5ba4b015c5a4d3ea0086e4e57352dd6
Brian Eaton <brian.eaton@gm.com>
2024-08-08 14:19:53 -0400

    Add check for shared mem payload type

--- up-streamer/src/ustreamer.rs
+++ up-streamer/src/ustreamer.rs
@@ -712,6 +712,18 @@ impl UListener for ForwardingListener {
             FORWARDING_LISTENER_FN_ON_RECEIVE_TAG,
             &msg
         );
+
+        if msg.attributes.payload_format.enum_value_or_default() == up_rust::UPayloadFormat::UPAYLOAD_FORMAT_SHM {
+            debug!(
+                "{}:{}:{} Received message with type UPAYLOAD_FORMAT_SHM, which is not supported: {:#?}",
+                self.forwarding_id,
+                FORWARDING_LISTENER_TAG,
+                FORWARDING_LISTENER_FN_ON_RECEIVE_TAG,
+                &msg.attributes
+            );
+            return;
+        }
+
         if let Err(e) = self.sender.send(Arc::new(msg)).await {
             error!(
                 "{}:{}:{} Unable to send message to worker pool: {e:?}",
