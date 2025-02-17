27f908b7682c8a5a6ea244fa63902663b4d6446a
Oliver Thallmair <oliver.thallmair@valtech-mobility.com>
2025-02-17 09:54:20 +0100

    cargo fmt

--- up-streamer/tests/usubscription.rs
+++ up-streamer/tests/usubscription.rs
@@ -20,7 +20,7 @@ use usubscription_static_file::USubscriptionStaticFile;
 #[tokio::test(flavor = "multi_thread")]
 async fn usubscription_bad_data() {
     integration_test_utils::init_logging();
-    
+
     let utransport_foo: Arc<dyn UTransport> =
         Arc::new(UPClientFailingRegister::new("upclient_foo").await);
     let utransport_bar: Arc<dyn UTransport> =
